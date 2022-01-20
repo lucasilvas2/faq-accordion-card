b4220a2283f42519ec3a4dca58714511455ec63c
lucasilvas2 <lucasoliver1068@gmail.com>
2022-01-19 23:38:38 -0300

    Ajuste função de interação; Mudança background-image para uma imagem.

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function aparecer(id_question, id_answer, id_arrow){
+    var teste = document.getElementsByClassName('answer');
+    var teste_arrow = document.getElementsByClassName('arrow');
+    var teste_p = document.getElementsByClassName('p');
+    for(let i = 0; i < 5 ; i++){
+        teste[i].style.display = 'none';    
+        teste_arrow[i].style.transform = 'none';
+        teste_p[i].style.fontWeight = '400';
+        teste_p[i].style.color = '#4a4b5e';
+    }
+    
     let element_answer = document.getElementById(id_answer);
     let element_question = document.getElementById(id_question);
     let element_arrow = document.getElementById(id_arrow);

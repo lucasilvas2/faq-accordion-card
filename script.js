a529605d742ae3270f3042ba576144a8dffcb7af
lucasilvas2 <lucasoliver1068@gmail.com>
2022-01-27 16:39:12 -0300

    Animação box; Efeito mouse hover jquery;

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
+var p_selecionado;
 function aparecer(id_question, id_answer, id_arrow){
+    $('.box').animate({left: '-400px'});
+    
     var teste = document.getElementsByClassName('answer');
     var teste_arrow = document.getElementsByClassName('arrow');
     var teste_p = document.getElementsByClassName('p');
+    
     for(let i = 0; i < 5 ; i++){
         teste[i].style.display = 'none';    
         teste_arrow[i].style.transform = 'none';
         teste_p[i].style.fontWeight = '400';
         teste_p[i].style.color = '#4a4b5e';
-    }
-    
+    }   
+    p_selecionado = document.getElementById(id_question);
     let element_answer = document.getElementById(id_answer);
     let element_question = document.getElementById(id_question);
     let element_arrow = document.getElementById(id_arrow);
+    
+
     element_question.style.color = "#1d1e35";
     element_question.style.fontWeight = "700";
 
@@ -19,4 +25,19 @@ function aparecer(id_question, id_answer, id_arrow){
 
     element_answer.style.display = "block";
     element_answer.style.marginTop = "10px";
+
+
 }
+
+
+$('.active').hover(function(){
+        $(this).css('color', '#f47c57');
+        $(this).css('cursor', 'pointer');
+    }, function(){
+        $(this).css('color', '#787887');
+        $(p_selecionado).css('color', '#1d1e35');
+        $(p_selecionado).css('fontWeigth', '700');
+    }
+);
+
+
